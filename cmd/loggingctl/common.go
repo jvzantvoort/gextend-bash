f853de0ff1f0d2e3779a9483db7ce006a51c474d
John van Zantvoort <john@vanzantvoort.org>
2024-07-14 19:09:34 +0200

    new: add loggingctl command

---
+++ cmd/loggingctl/common.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jvzantvoort/gextend-bash/logging"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func GetString(cmd cobra.Command, name string) string {
+	retv, _ := cmd.Flags().GetString(name)
+	if len(retv) != 0 {
+		log.Infof("Found %s as %s", name, retv)
+	}
+	return retv
+}
+
+func dump(data interface{}) {
+	b, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Print(string(b))
+}
+
+func handleLogCmd(cmd *cobra.Command, args []string) {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	log.Debugf("%s: start", cmd.Use)
+	defer log.Debugf("%s: end", cmd.Use)
+	filepath := "/home/jvzantvoort/Logs/2024/July/common.log"
+
+	logmsgs := logging.NewLogMessages(filepath)
+	for _, msg := range logmsgs.Messages {
+		tag := ""
+		if len(msg.Tag) != 0 {
+			tag = fmt.Sprintf("<%s> ", msg.Tag)
+		}
+		fmt.Printf("%s %-8s %s%s\n",
+			msg.Time.Format("2006-01-02 15:04:05"),
+			msg.Priority,
+			tag,
+			msg.Message,
+		)
+	}
+	/*
+		for _, msg := range logmsgs.messages {
+
+			fmt.Printf("%#v\n", msg)
+
+		}
+	*/
+}
