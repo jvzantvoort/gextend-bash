c6dc2a8412389912f30e838744e4a7de0aa570db
John van Zantvoort <john@vanzantvoort.org>
2026-05-02 19:29:20 +0200

    feat: update achelper content

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-#===============================================================================
-#
-#         FILE:  build.sh
-#
-#        USAGE:  build.sh
-#
-#  DESCRIPTION:  Wrap the go build commands
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  jvzantvoort (John van Zantvoort), john@vanzantvoort.org
-#      COMPANY:  JDC
-#      CREATED:  2024-07-12
-#
-# Copyright (C) 2024 John van Zantvoort
-#
-#===============================================================================
+
 C_BUILDLIST=$(cat <<-END
 linux  amd64
 linux  386
@@ -45,7 +27,6 @@ Build:
 HELP
 )
 
-# constants {{{
 C_SCRIPTPATH="$(readlink -f "$0")"
 C_SCRIPTDIR="$(dirname "$C_SCRIPTPATH")"
 C_BUILDDIR="${C_SCRIPTDIR}/build"
@@ -61,23 +42,20 @@ C_MSG_FORMAT="%-60s [ %-7s ]\n"
 #shellcheck disable=SC2059
 C_MSG_LEN="$(printf "${C_MSG_FORMAT}" "x" "x")"
 C_MSG_LEN="${#C_MSG_LEN}"
-C_MSG_LEN="$((C_MSG_LEN - 12))"
+C_MSG_LEN="$((C_MSG_LEN-12))"
 
 readonly C_SCRIPTPATH
 readonly C_SCRIPTDIR
 readonly C_BUILDDIR
 
 LDFLAGS=""
-[[ -n "${C_VERSION}"  ]] && LDFLAGS="${LDFLAGS} -X main.version=${C_VERSION}"
+[[ -n "${C_VERSION}" ]]  && LDFLAGS="${LDFLAGS} -X main.version=${C_VERSION}"
 [[ -n "${C_REVISION}" ]] && LDFLAGS="${LDFLAGS} -X main.revision=${C_REVISION}"
 LDFLAGS="${LDFLAGS} -w -s"
 
-# }}}
-
-# messages {{{
 #shellcheck disable=SC2034
-function strrep() {
-  local num="$1"
+strrep() {
+local num="$1"
   local re
 
   re='^[0-9]+$'
@@ -89,14 +67,14 @@ function strrep() {
   fi
 }
 
-function getpad() {
+getpad() {
   local msg="$1"
-  pad="${#msg}"              # length of the string
-  pad="$((C_MSG_LEN - pad))" # subtract it from then screen width
+  pad="${#msg}"
+  pad="$((C_MSG_LEN - pad))" 
   strrep "${pad}"
 }
 
-function print_msg() {
+print_msg() {
   local state=$1
   shift
   local msg="$*"
@@ -104,29 +82,32 @@ function print_msg() {
 
   padstr="$(getpad "${msg}")"
 
-  msg="${msg} $(strrep "${padstr}")" # create padding
+  msg="${msg} $(strrep "${padstr}")"
 
   #shellcheck disable=SC2059
   printf "${C_MSG_FORMAT}" "$msg ${padstr}" "${state}"
 }
-function print_title() {
+
+print_title() {
   local msg
   msg="$(echo "$@" | tr "[:lower:]" "[:upper:]")"
   print_msg "       " "$msg"
 }
-function print_subtitle() {
+
+print_subtitle() {
   local msg
   msg="$(echo "$@" | sed 's/.*/\L&/; s/[a-z]*/\u&/g')"
   print_msg "-------" "$msg"
 }
-function print_ok() { print_msg "SUCCESS" "$@"; }
-function print_nok() { print_msg "FAILURE" "$@"; }
-function print_fatal() { print_msg "FATAL" "$@"; }
-function print_warning() { print_msg "WARNING" "$@"; }
-function print_unknown() { print_msg "UNKNOWN" "$@"; }
-function print_skipped() { print_msg "SKIPPED" "$@"; }
 
-function test_result() {
+print_ok()      { print_msg "SUCCESS" "$@"; }
+print_nok()     { print_msg "FAILURE" "$@"; }
+print_fatal()   { print_msg "FATAL"   "$@"; }
+print_warning() { print_msg "WARNING" "$@"; }
+print_unknown() { print_msg "UNKNOWN" "$@"; }
+print_skipped() { print_msg "SKIPPED" "$@"; }
+
+test_result() {
   local retv=$1
   local message=$2
   if [[ "${retv}" == "0" ]]; then
@@ -138,7 +119,7 @@ function test_result() {
   fi
 }
 
-function test_fatal() {
+test_fatal() {
   local retv=$1
   local message=$2
   if [[ "${retv}" == "0" ]]; then
@@ -149,35 +130,28 @@ function test_fatal() {
   fi
 }
 
-function die() { test_fatal 1 "FATAL: $1"; }
+die() { test_fatal 1 "FATAL: $1"; }
 
-# }}}
-
-# listings {{{
-function list_gofiles() {
+list_gofiles() {
   pushd "${C_SCRIPTDIR}" >/dev/null 2>&1 || die "cannot changedir to scriptdir"
   find "." -type f -name '*.go' -not -path '*/vendor/*'
   popd >/dev/null 2>&1 || die "cannot changedir back"
 }
 
-function list_binaries() {
+list_binaries() {
   pushd "${C_SCRIPTDIR}" >/dev/null 2>&1 || die "cannot changedir to scriptdir"
   find cmd -maxdepth 1 -mindepth 1 -type d -printf "%f\n"
   popd >/dev/null 2>&1 || die "cannot changedir back"
 }
 
-# }}}
-
-function __calcdest() {
+__calcdest() {
   local goos="$1"
   local arch="$2"
   local target="$3"
   printf "%s/%s/%s/%s\n" "${C_BUILDDIR}" "${goos}" "${arch}" "${target}"
 }
 
-# actions {{{
-
-function action_fmt() {
+action_fmt() {
   list_gofiles | while read -r target; do
     goimports -w "${target}"
     test_result "$?" "reformat ${target}"
@@ -189,12 +163,12 @@ function action_fmt() {
   echo
 }
 
-function action_tags() {
+action_tags() {
   list_gofiles | xargs gotags >"${C_TAGSPATH}"
   test_result "$?" "Generate c-tags"
 }
 
-function action_build() {
+action_build() {
   local goos="$1"
   local arch="$2"
   local dest
@@ -216,7 +190,7 @@ function action_build() {
   test_fatal "${exitcode}" "build results"
 }
 
-function action_dependencies() {
+action_dependencies() {
   local msg
 
   pushd "${C_SCRIPTDIR}" >/dev/null 2>&1 || die "cannot changedir to scriptdir"
@@ -242,7 +216,7 @@ function action_dependencies() {
   popd >/dev/null 2>&1 || die "cannot changedir back"
 }
 
-function action_install() {
+action_install() {
   local goos="$1"
   local arch="$2"
   local bindir
@@ -256,7 +230,7 @@ function action_install() {
   done < <(list_binaries)
 }
 
-function action_package() {
+action_package() {
   local goos="$1"
   local arch="$2"
   local dest
@@ -283,7 +257,7 @@ function action_package() {
 
 }
 
-function action_createpackages() {
+action_createpackages() {
   action_dependencies
   while read -r goos arch; do
     print_subtitle "create ${goos}/${arch} archive"
@@ -292,7 +266,7 @@ function action_createpackages() {
   done < <(echo "${C_BUILDLIST}")
 }
 
-function action_cleanup() {
+action_cleanup() {
 
   [[ -d "${C_BUILDDIR}" ]] && rm -rvf "${C_BUILDDIR}"
   [[ -e "${C_PACKAGESPATH}" ]] && rm -vrf "${C_PACKAGESPATH}"
@@ -300,16 +274,13 @@ function action_cleanup() {
 
 }
 
-function action_check() {
+action_check() {
   pushd "${C_SCRIPTDIR}" >/dev/null 2>&1 || die "cannot changedir to scriptdir"
 
   go vet ./...
   test_result "$?" "go vet"
 
-  local goroot
-  goroot="$(go env GOROOT)"
-
-  golangci-lint run --exclude-dirs "${goroot}" ./...
+  golangci-lint run ./...
   test_result "$?" "golangci-lint"
 
   staticcheck ./...
@@ -319,7 +290,7 @@ function action_check() {
 
 }
 
-function action_list() {
+action_list() {
   pushd "${C_SCRIPTDIR}" >/dev/null 2>&1 || die "cannot changedir to scriptdir"
 
   find "${C_SCRIPTDIR}" -mindepth 1 -maxdepth 1 -type d \
@@ -333,70 +304,37 @@ function action_list() {
 
 }
 
-# }}}
+do_fmt()          { print_title "format the sources"; action_fmt; }
+do_tags()         { print_title "generate tags"; action_tags; }
+do_cleanup()      { print_title "cleanup"; action_cleanup; }
+do_dependencies() { print_title "update dependencies"; action_dependencies; }
+do_install()      { print_title "install locally"; action_install "$(go env GOOS)" "$(go env GOARCH)"; }
+do_package()      { print_title "create packages"; action_createpackages; }
+do_check()        { print_title "check the sources"; action_check; }
+do_list()         { action_list; }
 
-# interfaces {{{
-function do_fmt() {
-  print_title "format the sources"
-  action_fmt
-}
-function do_tags() {
-  print_title "generate tags"
-  action_tags
-}
-function do_cleanup() {
-  print_title "cleanup"
-  action_cleanup
-}
-function do_dependencies() {
-  print_title "update dependencies"
-  action_dependencies
-}
-function do_build() {
+do_build() {
   print_title "build ${goos}/${arch}"
   action_dependencies
   action_build "$(go env GOOS)" "$(go env GOARCH)"
 }
-function do_install() {
-  print_title "install locally"
-  action_install "$(go env GOOS)" "$(go env GOARCH)"
-}
-function do_package() {
-  print_title "create packages"
-  action_createpackages
-}
-function do_usage() {
+do_usage() {
   printf "USAGE:\n\n  %s <option>\n\n" "${C_SCRIPTPATH}"
   echo "${C_HELPMSG}"
   printf "\n\n"
   exit 0
 }
-function do_check() {
-  print_title "check the sources"
-  action_check
-}
-function do_list() { action_list; }
-# }}}
-
-#------------------------------------------------------------------------------#
-#                                    Main                                      #
-#------------------------------------------------------------------------------#
 
 case "$1" in
-fmt) do_fmt ;;
-tags) do_tags ;;
-build) do_build ;;
-update) do_dependencies ;;
-install) do_install ;;
-package) do_package ;;
-cleanup) do_cleanup ;;
-check) do_check ;;
-list) do_list ;;
-help) do_usage ;;
-*) do_usage ;;
+  fmt)     do_fmt          ;;
+  tags)    do_tags         ;;
+  build)   do_build        ;;
+  update)  do_dependencies ;;
+  install) do_install      ;;
+  package) do_package      ;;
+  cleanup) do_cleanup      ;;
+  check)   do_check        ;;
+  list)    do_list         ;;
+  help)    do_usage        ;;
+  *)       do_usage        ;;
 esac
-
-#------------------------------------------------------------------------------#
-#                                  The End                                     #
-#------------------------------------------------------------------------------#
-# vim: foldmethod=marker
--- cmd/achelper/main.go
+++ cmd/achelper/main.go
@@ -2,16 +2,35 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"os"
 )
 
+//go:embed message/*
+var Content embed.FS
+
+func AbortMe() {
+	msgstr, err := Content.ReadFile("message/abort")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err)
+		msgstr = []byte("undefined")
+	}
+	fmt.Fprintf(os.Stderr, string(msgstr))
+	os.Exit(1)
+}
+
 func main() {
-	firstArgument := os.Args[1]
-	if firstArgument == "ssh" {
+	if len(os.Args) == 1 {
+		AbortMe()
+	}
+
+	if os.Args[1] == "ssh" {
 		err := SecureShellHelper()
 		if err != nil {
 			fmt.Println(err)
 		}
+	} else {
+		AbortMe()
 	}
 }
